12fad842976691666c4d35ce7ca2bcfbd9135eb3
Ghanendra <Ghanen179@gmail.com>
2025-01-20 11:21:17 +0700

    Menambahkan fitur update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'home.dart';
 
 void main() async{
